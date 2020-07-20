3ddd9d90ea0ec8910b285e17364bee74a3ab2daa
dnsgk4983 <dnsgk4983@gmail.com>
2020-07-20 17:11:06 +0900

    0720

--- html/js/main.js
+++ html/js/main.js
@@ -647,3 +647,13 @@ $(function(){
 		$(this).children('ul').stop().slideToggle();
 	});
 });
+var maxIndex = $("li.graph-bar").length;
+
+for(var i=0; i<maxIndex; i++){
+  var val = $("li.graph-bar").eq(i).attr('graph-val');
+  $("li.graph-bar").eq(i).css({
+    "left": (i+1)*80+"px"
+  }).animate({
+    "height":val+"%"
+  },800);
+}
